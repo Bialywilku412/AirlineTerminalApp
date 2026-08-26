380926f23fffdca10c091ecdd446c58033b308b6
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-08-26 09:36:40 +0200

    Logic for Seats

---
+++ Airline/BLL/SeatService.cs
@@ -0,0 +1,18 @@
+﻿public class SeatService
+{
+    private readonly SeatRepository _repository;
+
+    public SeatService(SeatRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public Result<List<Seat>> GetSeatsByPlaneId(int id)
+    {
+        List<Seat> seats = _repository.GetSeatsByPlaneId(id);
+        if (seats.Count == 0)
+            return new Result<List<Seat>>(false, "No seats found");
+
+        return new Result<List<Seat>>(true, "succes", seats);
+    }
+}
---
+++ Airline/DAL/SeatRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+
+public class SeatRepository
+{
+    private readonly string _connectionString = "Data Source=database.db";
+
+    public List<Seat> GetSeatsByPlaneId(int id)
+    {
+        using var connection = new SqliteConnection(_connectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            SELECT * FROM Seats
+            WHERE id = PlaneId
+        ";
+
+        List<Seat> seats = new();
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            Seat seat = new Seat
+            {
+                Id = reader.GetInt32(0),
+                PlaneId = reader.GetInt32(1),
+                Row = reader.GetInt32(2),
+                Column = reader.GetChar(3),
+                Class = Enum.Parse<SeatClass>(reader.GetString(4))
+            };
+            seats.Add(seat);
+        }
+        return seats;
+    }
+}
