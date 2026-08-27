117a6f82838dd4dd5a95a71f77f649233030bbf7
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-08-27 10:00:25 +0200

    repositories and services set in program

--- Airline/Program.cs
+++ Airline/Program.cs
@@ -1,7 +1,20 @@
-﻿PlaneRepository _repository = new PlaneRepository();
-PlaneService _service = new PlaneService(_repository);
+﻿PlaneRepository _planeRepository = new PlaneRepository();
+PlaneService _planeService = new PlaneService(_planeRepository);
+
+UserRepository _userRepository = new UserRepository();
+UserService _userService = new UserService(_userRepository);
+
+FlightRepository _flightRepository = new FlightRepository();
+FlightService _flightService = new FlightService(_flightRepository, _planeService);
+
+SeatRepository _seatRepository = new SeatRepository();
+SeatService _seatService = new SeatService(_seatRepository);
+
+SeatReservationRepository _seatReservationRepository = new SeatReservationRepository();
+SeatReservationService _seatReservationService = new SeatReservationService(_seatReservationRepository);
+
 new DatabaseInitializer().Initialize();
-if(_service.ShowAllPlanes().Success == false)
+if(_planeService.ShowAllPlanes().Success == false)
 {
     new DatabaseInitializer().AddPlane("Boeing 737", 195);
     new DatabaseInitializer().AddPlane("Airbus 330", 345);
@@ -9,4 +22,4 @@ if(_service.ShowAllPlanes().Success == false)
 }
 new DatabaseInitializer().SeedSeats(1);
 
-LoginMenu.ShowLoginMenu();
+LoginMenu.ShowLoginMenu(_planeService, _userService, _flightService, _seatService, _seatReservationService);
