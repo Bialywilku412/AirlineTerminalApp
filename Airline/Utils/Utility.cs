a8cb48b3682ccab67b56690b63d65d0bd72be24d
Bialywilku412 <95282844+Bialywilku412@users.noreply.github.com>
2026-08-31 11:06:47 +0200

    added utils for reusing

---
+++ Airline/Utils/Utility.cs
@@ -0,0 +1,15 @@
+﻿using Spectre.Console;
+
+public static class Utility
+{
+    public static void Return()
+    {
+        var choice = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .AddChoices("Return"));
+
+        if (choice != null)
+            Console.Clear();
+            return;
+    }
+}
